829ee3b26d179df0d71e7897de0eede4cd7d7e42
Marissa Honore <marissahonore@kierres-macbook-pro.local>
2017-07-26 14:36:12 -0500

    Initial

---
+++ AutoLayout/MapViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MapViewController.swift
+//  AutoLayout
+//
+//  Created by Marissa Honore on 7/26/17.
+//  Copyright © 2017 mhonore. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation: CLLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        
+        let longitude = userLocation.coordinate.longitude
+        
+        let latDelta: CLLocationDegrees = 0.05
+        
+        let lonDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.map.setRegion(region, animated: true)
+        
+        
+    }
+}
